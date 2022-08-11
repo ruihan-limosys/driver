0f44520a4309238bcde1be37768b7aee9684f004
Rui Han <rui@Mikes-iMac-Pro.local>
2022-08-11 15:37:38 -0400

    first commit

---
+++ src/MapView.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState} from 'react';
+import { View, StyleSheet } from 'react-native';
+import RNMapView, { Circle, Marker,Polyline } from 'react-native-maps';
+
+const MapView = ({ coords }) => {
+  const mapRef = useRef(null);
+  const [routeCoords,setRouteCoords] = useState([])
+  useEffect(() => {
+    if (!!coords && mapRef.current) {
+      setRouteCoords([...routeCoords,coords])
+      mapRef.current.animateCamera({
+        center: {
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        },
+        pitch: 0,
+        heading: 0,
+        altitude: 1000,
+        zoom: 16,
+      });
+    }
+  }, [coords]);
+
+  return (
+    <View style={styles.container}>
+      <RNMapView
+        ref={mapRef}
+        initialCamera={{
+          altitude: 15000,
+          center: {
+            latitude: 23.7603,
+            longitude: 90.4125,
+          },
+          heading: 0,
+          pitch: 0,
+          zoom: 11,
+        }}
+        loadingEnabled
+        loadingBackgroundColor="white"
+        style={StyleSheet.absoluteFillObject}
+        rotateEnabled={false}
+      >
+        {!!coords && (
+          <>
+          <Polyline coordinates={routeCoords} strokeWidth={5} />
+            <Marker
+              anchor={{ x: 0.5, y: 0.6 }}
+              coordinate={{
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+              }}
+              flat
+              style={{
+                ...(coords.heading !== -1 && {
+                  transform: [
+                    {
+                      rotate: `${coords.heading}deg`,
+                    },
+                  ],
+                }),
+              }}
+            >
+              <View style={styles.dotContainer}>
+                <View style={[styles.arrow]} />
+                <View style={styles.dot} />
+              </View>
+            </Marker>
+            <Circle
+              center={{
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+              }}
+              radius={coords.accuracy}
+              strokeColor="rgba(0, 150, 255, 0.5)"
+              fillColor="rgba(0, 150, 255, 0.5)"
+            />
+          </>
+        )}
+      </RNMapView>
+    </View>
+  );
+};
+
+export default MapView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  dotContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dot: {
+    backgroundColor: 'rgb(0, 120, 255)',
+    width: 24,
+    height: 24,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 12,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 1.5,
+    elevation: 4,
+  },
+  arrow: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 6,
+    borderRightWidth: 6,
+    borderBottomWidth: 10,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'rgb(0, 120, 255)',
+  },
+});
